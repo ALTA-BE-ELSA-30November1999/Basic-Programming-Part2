7b2e73f4ed65c92e95eabc7e08876b24b7aacea3
Elsa <elsafirmaniar5@gmail.com>
2023-09-28 00:51:32 +0700

    Solve Problem

--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,9 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+Input_Harga = 370000
+Input_Diskon = 15/100
+Harga = Input_Diskon * Input_Harga
+Harga_akhir = Input_Harga - Harga
+print("Harga yang harus dibayar adalah Rp.", "{:,}".format(round(Harga_akhir)))
+# print("{:,}".format(round(Harga_akhir)))
